252e40325698f7dbffedcaf155eb7c44bfc04999
KCCI <kabalah_qbl@naver.com>
2018-04-24 15:53:33 +0900

    ProfSettings 수정중입니다

--- ToFast.Data/ToFast/Forms/ProfSetting.cs
+++ ToFast.Data/ToFast/Forms/ProfSetting.cs
@@ -10,6 +10,11 @@ using System.Windows.Forms;
 
 namespace ToFast
 {
+    /// <summary>
+    /// 작성자 : 박진수
+    /// 작성일 : 04-24 15:50
+    /// 수정내용 : 알람인원 수정시 코드 작성중
+    /// </summary>
     public partial class ProfSetting : Form
     {
         public ProfSetting()
