8bdbbfb4bd12c0df26de368e3b294776afed66bb
sunlainer <cpfls77@nate.com>
2018-04-24 12:33:16 +0900

    ProjectCreate =>DataRepergytorry

---
+++ ToFast.Data/ToFast.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToFast.Data
+{
+    public class Class1
+    {
+    }
+}
