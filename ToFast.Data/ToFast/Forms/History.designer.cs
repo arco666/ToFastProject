b386d277155b38ac5ececab1862bb36567771e6d
jeahong so <woghd4545@gmail.com>
2018-04-25 14:38:08 +0900

    빌드오류 수정

--- ToFast.Data/ToFast/Forms/History.designer.cs
+++ ToFast.Data/ToFast/Forms/History.designer.cs
@@ -31,34 +31,22 @@
 			this.components = new System.ComponentModel.Container();
 			this.dataGridView = new System.Windows.Forms.DataGridView();
 			this.studentBindingSource = new System.Windows.Forms.BindingSource(this.components);
-			this.toFastDataSet = new ToFast.ToFastDataSet();
 			this.btnDelete = new System.Windows.Forms.Button();
 			this.btnOk = new System.Windows.Forms.Button();
 			this.tbInfo = new System.Windows.Forms.TextBox();
-			this.studentTableAdapter = new ToFast.ToFastDataSetTableAdapters.StudentTableAdapter();
 			this.toFastDataSetBindingSource = new System.Windows.Forms.BindingSource(this.components);
-			this.toFastDataSet1 = new ToFast.ToFastDataSet1();
 			this.questionIndexBindingSource = new System.Windows.Forms.BindingSource(this.components);
-			this.questionIndexTableAdapter = new ToFast.ToFastDataSet1TableAdapters.QuestionIndexTableAdapter();
-			this.toFastDataSet2 = new ToFast.ToFastDataSet2();
 			this.questionIndexBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
-			this.questionIndexTableAdapter1 = new ToFast.ToFastDataSet2TableAdapters.QuestionIndexTableAdapter();
-			this.toFastDataSet3 = new ToFast.ToFastDataSet3();
 			this.questionIndexBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
-			this.questionIndexTableAdapter2 = new ToFast.ToFastDataSet3TableAdapters.QuestionIndexTableAdapter();
 			this.QuestionTime = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Context = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.SubjectName = new System.Windows.Forms.DataGridViewTextBoxColumn();
 			this.Checkable = new System.Windows.Forms.DataGridViewCheckBoxColumn();
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.toFastDataSetBindingSource)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet1)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.questionIndexBindingSource)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet2)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.questionIndexBindingSource1)).BeginInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet3)).BeginInit();
 			((System.ComponentModel.ISupportInitialize)(this.questionIndexBindingSource2)).BeginInit();
 			this.SuspendLayout();
 			// 
@@ -81,12 +69,9 @@
 			// studentBindingSource
 			// 
 			this.studentBindingSource.DataMember = "Student";
-			this.studentBindingSource.DataSource = this.toFastDataSet;
 			// 
 			// toFastDataSet
 			// 
-			this.toFastDataSet.DataSetName = "ToFastDataSet";
-			this.toFastDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
 			// 
 			// btnDelete
 			// 
@@ -123,54 +108,34 @@
 			// 
 			// studentTableAdapter
 			// 
-			this.studentTableAdapter.ClearBeforeFill = true;
 			// 
 			// toFastDataSetBindingSource
 			// 
-			this.toFastDataSetBindingSource.DataSource = this.toFastDataSet;
 			this.toFastDataSetBindingSource.Position = 0;
 			// 
 			// toFastDataSet1
 			// 
-			this.toFastDataSet1.DataSetName = "ToFastDataSet1";
-			this.toFastDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
 			// 
 			// questionIndexBindingSource
 			// 
 			this.questionIndexBindingSource.DataMember = "QuestionIndex";
-			this.questionIndexBindingSource.DataSource = this.toFastDataSet1;
 			// 
 			// questionIndexTableAdapter
 			// 
-			this.questionIndexTableAdapter.ClearBeforeFill = true;
 			// 
 			// toFastDataSet2
 			// 
-			this.toFastDataSet2.DataSetName = "ToFastDataSet2";
-			this.toFastDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
 			// 
 			// questionIndexBindingSource1
 			// 
 			this.questionIndexBindingSource1.DataMember = "QuestionIndex";
-			this.questionIndexBindingSource1.DataSource = this.toFastDataSet2;
-			// 
-			// questionIndexTableAdapter1
-			// 
-			this.questionIndexTableAdapter1.ClearBeforeFill = true;
-			// 
-			// toFastDataSet3
-			// 
-			this.toFastDataSet3.DataSetName = "ToFastDataSet3";
-			this.toFastDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
 			// 
 			// questionIndexBindingSource2
 			// 
 			this.questionIndexBindingSource2.DataMember = "QuestionIndex";
-			this.questionIndexBindingSource2.DataSource = this.toFastDataSet3;
 			// 
 			// questionIndexTableAdapter2
 			// 
-			this.questionIndexTableAdapter2.ClearBeforeFill = true;
 			// 
 			// QuestionTime
 			// 
@@ -213,13 +178,9 @@
 			this.Load += new System.EventHandler(this.History_Load);
 			((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.studentBindingSource)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.toFastDataSetBindingSource)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet1)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.questionIndexBindingSource)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet2)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.questionIndexBindingSource1)).EndInit();
-			((System.ComponentModel.ISupportInitialize)(this.toFastDataSet3)).EndInit();
 			((System.ComponentModel.ISupportInitialize)(this.questionIndexBindingSource2)).EndInit();
 			this.ResumeLayout(false);
 			this.PerformLayout();
@@ -232,19 +193,11 @@
         private System.Windows.Forms.Button btnDelete;
         private System.Windows.Forms.Button btnOk;
         private System.Windows.Forms.TextBox tbInfo;
-		private ToFastDataSet toFastDataSet;
 		private System.Windows.Forms.BindingSource studentBindingSource;
-		private ToFastDataSetTableAdapters.StudentTableAdapter studentTableAdapter;
 		private System.Windows.Forms.BindingSource toFastDataSetBindingSource;
-		private ToFastDataSet1 toFastDataSet1;
 		private System.Windows.Forms.BindingSource questionIndexBindingSource;
-		private ToFastDataSet1TableAdapters.QuestionIndexTableAdapter questionIndexTableAdapter;
-		private ToFastDataSet2 toFastDataSet2;
 		private System.Windows.Forms.BindingSource questionIndexBindingSource1;
-		private ToFastDataSet2TableAdapters.QuestionIndexTableAdapter questionIndexTableAdapter1;
-		private ToFastDataSet3 toFastDataSet3;
 		private System.Windows.Forms.BindingSource questionIndexBindingSource2;
-		private ToFastDataSet3TableAdapters.QuestionIndexTableAdapter questionIndexTableAdapter2;
 		private System.Windows.Forms.DataGridViewTextBoxColumn QuestionTime;
 		private System.Windows.Forms.DataGridViewTextBoxColumn Context;
 		private System.Windows.Forms.DataGridViewTextBoxColumn SubjectName;
